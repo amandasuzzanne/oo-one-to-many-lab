ba4c0a16085097a3c8e96a71cd354420b6f7ecaa
amandasuzzanne <amandasuzanne12@gmail.com>
2024-06-19 13:07:16 +0300

    Complete lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,31 @@
-class Pet:
+class Owner:
+    def __init__(self, name) -> None:
+        self.name = name
+        pass
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise Exception(f"{pet} is not a Pet")
+    
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
+    
     pass
 
-class Owner:
-    pass
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all =[]
+    def __init__(self, name, pet_type, owner=None) -> None:
+        if pet_type in Pet.PET_TYPES:
+            self.pet_type = pet_type
+        else:
+            raise Exception(f"{pet_type} is not a valid pet type")
+        self.name = name
+        self.owner = owner
+        self.all.append(self)
+      
